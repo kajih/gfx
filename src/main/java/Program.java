6a4738a8be1c3f30c02f13f8b51aab8a18ce17c7
JH GDesk@SC <johan.haggqvist@gmail.com>
2018-07-04 14:18:43 +0200

    Animation

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,7 +3,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Program extends JPanel {
+public class Program extends JPanel implements ActionListener {
+
+    private int cx = 0;
+    private int cy = 0;
+    private int rad = 200;
 
     @Override
     public void paintComponent(Graphics g) {
@@ -11,31 +15,40 @@ public class Program extends JPanel {
 
         setBackground(Color.black);
         setForeground(Color.white);
-        g.drawLine(10, 10, 200, 300);
+
+        //g.drawLine(10, 10, 200, 300);
+        g.drawOval(this.cx,this.cy, this.rad, this.rad);
+    }
+
+    private void drawScene() {
+        this.cx = (this.cx + 1) % 800;
+        repaint();
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        drawScene();
     }
 
     public static void main(String[] arg) {
 
-        int delay = 500; // milliseconds
-        ActionListener updateTask = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-            }
-        };
-        new Timer(delay, updateTask).start();
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame;
+            Program progClass = new Program();
 
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame;
-                frame = new JFrame("DNA Rockets");
-                frame.add(new Program());
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(1000, 800);
-                frame.setResizable(false);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-            }
+            frame = new JFrame("DNA Rockets");
+            frame.add(progClass);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1000, 800);
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+            int delay = 1; // milliseconds
+            Timer timer = new Timer(delay, progClass);
+            timer.setInitialDelay(0); // First timer is immediate.
+            timer.start();
+
         });
     }
 }
